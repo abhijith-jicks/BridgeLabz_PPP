af18e0cb1fe52fd13d597a0cc7591318c4767b41
Anujith421 <anujithjicks614@gmail.com>
2025-08-11 12:07:09 +0530

    Added CeltoFah.java in JavaBasics

---
+++ JavaBasics/CeltoFah.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class CeltoFah {
+    public static  void main(string[] args){
+        Scanner sc=new Scanner(System.in);
+        int c=sc.nextInt();
+        double f=(c*(9/5))+32;
+        System.out.println(f);
+    }
+}
